6afa5c378f010da88a87bbe7fb41a07fb1f9073b
rizvana-24 <mohammadrizvana24@gmail.com>
2026-09-12 07:17:43 +0530

    Fix SiteVisitsRoutes filename

--- crm backend/src/controllers/reportController.js
+++ crm backend/src/controllers/reportController.js
@@ -3,7 +3,7 @@ import Property from "../models/Property.js";
 import Lead from "../models/Lead.js";
 import Appointment from "../models/Appointment.js";
 import Enquiry from "../models/Enquiry.js";
-import Sitevisit from "../models/Sitevisit.js";
+import Sitevisit from "../models/SiteVisit.js";
 export const dashboard = async (
   req,
   res
